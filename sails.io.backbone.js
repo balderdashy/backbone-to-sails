95234c5ad1ee08e67e564b51b844d3301dd70930
Mike McNeil <michael.r.mcneil@gmail.com>
2013-12-01 01:08:22 -0600

    Updated to add comet listener and hacky little maxRetries deal

--- sails.io.backbone.js
+++ sails.io.backbone.js
@@ -11,123 +11,218 @@
  * Copyright(c) 2011 Jake Luer <jake@alogicalparadox.com>
  * MIT Licensed
  */
- 
 
-/**
- * # Backbone.sync
- *
- * Replaces default Backbone.sync function with socket.io transport
- * @name sync
- */
-Backbone.sync = function (method, model, options) {
+(function () {
 
-	// Clone options to avoid smashing anything unexpected
-	options = _.extend({}, options);
+
+	// The active `socket`
+	var socket;
 
 
 
-	// Get the actual URL (call `.url()` if it's a function)
-	var url;
-	if (options.url) {
-		url = _.result(options, 'url');
-	}
-	else if (model.url) {
-		url = _.result(model, 'url');
-	}
-	// Throw an error when a URL is needed, and none is supplied.
-	// Copied from backbone.js#1558
-	else throw new Error('A "url" property or function must be specified');
+	// Also keep track of where it came from
+	var socketSrc;
 
 
 
-	// Build parameters to send to the server
-	var params = {};
+	/**
+	 * _acquireSocket()
+	 *
+	 * Grab hold of our active socket object, set it on `socket` closure variable above.
+	 * (if your connected socket exists on a non-standard variable, change here)
+	 *
+	 * @api private
+	 */
+	var _acquireSocket = function ( ) {
+		if (socket) return;
 
-	if ( !options.data && model ) {
-		params = options.attrs || model.toJSON(options) || {};
-	}
+		if (Backbone.socket) {
+			socket = Backbone.socket;
+			socketSrc = '`Backbone.socket`';
+		}
+		else if (window.socket) {
+			socket = window.socket;
+			socketSrc = '`window.socket`';
+		}
 
-	if (options.patch === true && options.data.id === null && model) {
-		params.id = model.id;
-	}
-
-
-
-	// Locate the active `socket`
-	// (if your connected socket exists on a different variable, change here)
-	var socket, socketSrc;
-	if (model.socket) {
-		socket = model.socket;
-		socketSrc = 'the `socket` property of one of its models';
-	}
-	else if (Backbone.socket) {
-		socket = model.socket;
-		socketSrc = '`Backbone.socket`';
-	}
-	else if (window.socket) {
-		socket = window.socket;
-		socketSrc = '`window.socket`';
-	}
-	else throw new Error(
-		'\n' +
-		'Backbone cannot find a suitable `socket` object.\n' +
-		'This SDK expects the active socket to be located at `window.socket`, '+
-		'`Backbone.socket` or the `socket` property\n' +
-		'of the Backbone model or collection attempting to communicate w/ the server.\n'
-	);
+		// The first time a socket is acquired, bind comet listener
+		if (socket) _bindCometListener();
+	};
 
 
 
-	// Ensure the socket is connected and able to communicate w/ the server.
-	if ( !socket.socket || !socket.socket.connected ) throw new Error(
-		'\n' +
-		'Backbone is trying to communicate with the Sails server using '+ socketSrc +',\n'+
-		'but it\'s `connected` property is still set to false.\n' +
-		'But maybe Socket.io just hasn\'t finished connecting yet?\n' +
-		'\n' +
-		'You might check to be sure you\'re waiting for `socket.on(\'connect\')`\n' +
-		'before using sync methods on your Backbone models and collections.'
-	);
+	// Set up `async.until`-esque mechanism which will attempt to acquire a socket.
+	var attempts = 0,
+		maxAttempts = 3,
+		interval = 1500,
+		initialInterval = 250;
 
 
 
-	// Map Backbone's concept of CRUD methods to HTTP verbs
-	var verb;
-	switch (method) {
-		case 'create':
-			verb = 'post';
-			break;
-		case 'read':
-			verb = 'get';
-			break;
-		case 'update':
-			verb = 'put';
-			break;
-		default:
-			verb = method;
-	}
+	var _attemptToAcquireSocket = function () {
+		if ( socket ) return;
+		attempts++;
+		_acquireSocket();
+		if (attempts >= maxAttempts) return;
+		setTimeout(_attemptToAcquireSocket, interval);
+	};
 
 
 
-	// Send a simulated HTTP request to Sails via Socket.io
-	var simulatedXHR = 
-		socket.request(url, params, function serverResponded ( response ) {
-			if (options.success) options.success(response);
-		}, verb);
+	// Attempt to acquire the socket more quickly the first time,
+	// in case the user is on a fast connection and it's available.
+	setTimeout(_attemptToAcquireSocket, initialInterval);
 
 
 
-	return simulatedXHR;
-};
 
 
-	// TODO:
-	// Replace sails.io.js with `jQuery-to-sails.js`, which can be a prerequisite of 
-	// this SDK.
-	//
-	// Will allow for better client-side error handling, proper simulation of $.ajax,
-	// easier client-side support of headers, and overall a better experience.
 
+
+	/**
+	 * Backbone.on('comet', ...)
+	 *
+	 * Since Backbone is already a listener (extends Backbone.Events)
+	 * all we have to do is trigger the event on the Backbone global when
+	 * we receive a new message from the server.
+	 * 
+	 * I realize this doesn't do a whole lot right now-- that's ok.
+	 * Let's start light and layer on additional functionality carefully.
+	 */
+	var _bindCometListener = function socketAcquiredForFirstTime () {
+		socket.on('message', function cometMessageReceived (message) {
+			Backbone.trigger('comet', message);
+		});
+	};
+
+
+
+
+
+
+	/**
+	 * # Backbone.sync
+	 *
+	 * Replaces default Backbone.sync function with socket.io transport
+	 *
+	 * @param {String} method
+	 * @param {Backbone.Model|Backbone.Collection} model
+	 * @param {Object} options
+	 *
+	 * @name sync
+	 */
+	Backbone.sync = function (method, model, options) {
+
+		// Clone options to avoid smashing anything unexpected
+		options = _.extend({}, options);
+
+
+
+		// Get the actual URL (call `.url()` if it's a function)
+		var url;
+		if (options.url) {
+			url = _.result(options, 'url');
+		}
+		else if (model.url) {
+			url = _.result(model, 'url');
+		}
+		// Throw an error when a URL is needed, and none is supplied.
+		// Copied from backbone.js#1558
+		else throw new Error('A "url" property or function must be specified');
+
+
+
+		// Build parameters to send to the server
+		var params = {};
+
+		if ( !options.data && model ) {
+			params = options.attrs || model.toJSON(options) || {};
+		}
+
+		if (options.patch === true && options.data.id === null && model) {
+			params.id = model.id;
+		}
+
+
+
+		// If socket is not defined yet, try to grab it
+		_acquireSocket();
+
+
+
+		// Handle missing socket
+		if (!socket) {
+			throw new Error(
+				'\n' +
+				'Backbone cannot find a suitable `socket` object.\n' +
+				'This SDK expects the active socket to be located at `window.socket`, '+
+				'`Backbone.socket` or the `socket` property\n' +
+				'of the Backbone model or collection attempting to communicate w/ the server.\n'
+			);
+		}
+
+
+
+		// Ensure the socket is connected and able to communicate w/ the server.
+		if ( !socket.socket || !socket.socket.connected ) throw new Error(
+			'\n' +
+			'Backbone is trying to communicate with the Sails server using '+ socketSrc +',\n'+
+			'but it\'s `connected` property is still set to false.\n' +
+			'But maybe Socket.io just hasn\'t finished connecting yet?\n' +
+			'\n' +
+			'You might check to be sure you\'re waiting for `socket.on(\'connect\')`\n' +
+			'before using sync methods on your Backbone models and collections.'
+		);
+
+
+
+		// Map Backbone's concept of CRUD methods to HTTP verbs
+		var verb;
+		switch (method) {
+			case 'create':
+				verb = 'post';
+				break;
+			case 'read':
+				verb = 'get';
+				break;
+			case 'update':
+				verb = 'put';
+				break;
+			default:
+				verb = method;
+		}
+
+
+
+		// Send a simulated HTTP request to Sails via Socket.io
+		var simulatedXHR = 
+			socket.request(url, params, function serverResponded ( response ) {
+				if (options.success) options.success(response);
+			}, verb);
+
+
+
+		return simulatedXHR;
+	};
+
+
+
+
+
+
+		
+
+
+
+
+	/**
+	 * TODO:
+	 * Replace sails.io.js with `jQuery-to-sails.js`, which can be a prerequisite of 
+	 * this SDK.
+	 *
+	 * Will allow for better client-side error handling, proper simulation of $.ajax,
+	 * easier client-side support of headers, and overall a better experience.
+	 */
 	/*
 	var simulatedXHR = $.Deferred();
 
@@ -161,3 +256,4 @@ Backbone.sync = function (method, model, options) {
 	*/
 
 
+})();
